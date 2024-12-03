47d3ce323d6902242f5e1189437fc44ccd6b44f1
Lachezar Hristov <lachezar.hristov1@gmail.com>
2024-12-03 20:18:40 +0000

    fixed settings file

--- resume/settings.py
+++ resume/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whtitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'resume.urls'
